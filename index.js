71d7782e0ab14b5ccab7749e6d700a939d68a68a
Karim Matrah <karim.matrah@wifirst.fr>
2017-02-19 21:50:41 +0100

    do not start the server if APP_ID and API_KEY envvars are not provided

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-const server = require('./src/server')
-
-server.start()
+if (process.env.APP_ID && process.env.API_KEY) {
+  const server = require('./src/server')
+  server.start()
+} else {
+  console.error("You must provide an Application ID and an Admin API KEY.")
+}
